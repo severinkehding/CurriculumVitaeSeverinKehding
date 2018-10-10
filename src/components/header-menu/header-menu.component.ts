679aedec67f116efdc7012d4b276d143aededfd5
Severin Kehding <severinkehding@gmail.com>
2018-10-10 20:21:19 +0200

    CurriculumVitaeSeverinKehding-8 working piece of garbage needs some cleaning

--- src/components/header-menu/header-menu.component.ts
+++ src/components/header-menu/header-menu.component.ts
@@ -6,15 +6,18 @@
  * @creation    09-Jul-18
  *
  */
-import { Component } from '@angular/core';
+import { Component, Directive, ElementRef, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { MatDialog } from '@angular/material';
+import * as $ from 'jquery';
+import * as terminal from 'jquery.terminal';
 import { ShortcutHelpDialogComponent } from '../dialogs/shortcut-help-dialog/shortcut-help-dialog.component';
-
+declare var jQuery: any;
 @Component({
   selector: 'app-header-menu',
   templateUrl: './header-menu.component.html',
   styleUrls: ['./header-menu.component.scss']
 })
+
 export class HeaderMenuComponent {
   constructor(public dialog: MatDialog) {
   }
@@ -23,8 +26,74 @@ export class HeaderMenuComponent {
     const dialogRef = this.dialog.open(ShortcutHelpDialogComponent, {autoFocus: true, panelClass: 'shortcut-dialog-panel'});
 
     /** On Dialog close TODO: perhaps remove this because its not nesessarcy **/
-    dialogRef.afterClosed().subscribe(result => {
-      console.log(`Dialog result: ${result}`);
-    });
+
+      $(document).ready(function(){
+        console.log(terminal);
+        jQuery('.mat-dialog-container').terminal(function(command) {
+
+        });
+      });
+
+
+
+
+    /* dialogRef.afterClosed().subscribe(result => {
+       $('.mat-dialog-container').terminal(function(command) {
+         if (command !== '') {
+           var result = window.eval(command);
+           if (result != undefined) {
+             this.echo(String(result));
+           }
+         }
+       }, {
+         greetings: 'Javascript Interpreter',
+         name: 'js_demo',
+         height: 200,
+         width: 450,
+         prompt: 'js> '
+       });
+       console.log(`Dialog result: ${result}`);
+     });*/
+  }
+}
+
+
+@Directive({selector: 'canvas[m3Chart]'})
+export class ChartsDirective implements OnInit, OnChanges {
+
+  @Input() public chartType: string;
+  @Input() public options: any = {};
+  @Input() public data: any = {};
+
+  private chart: any;
+
+  constructor(private element: ElementRef) {
+  }
+
+  ngOnInit(): void {
+
+    this.createChart();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (this.chart) {
+
+      // Data for chart updated?
+      if (changes.hasOwnProperty('data')) {
+
+        // Overwrite chart data and update
+        Object.assign(this.chart.data, changes.data.currentValue);
+        this.chart.update();
+      }
+    }
+  }
+
+  private createChart() {
+    const options: any = Object.assign({}, this.options);
+   /* this.chart = new JQueryTerminalStatic(this.element.nativeElement, {
+      type: this.chartType,
+      data: {datasets: [{data: []}], labels: []},
+      options: options
+    });*/
   }
 }
--- src/components/header-menu/header-menu.module.ts
+++ src/components/header-menu/header-menu.module.ts
@@ -2,16 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { ButtonModule } from '../button/button.module';
-import { HeaderMenuComponent } from './header-menu.component';
+import { ChartsDirective, HeaderMenuComponent } from './header-menu.component';
 import { ShortcutHelpDialogModule } from '../dialogs/shortcut-help-dialog/shortcut-help-dialog.module';
 import { ShortcutHelpDialogComponent } from '../dialogs/shortcut-help-dialog/shortcut-help-dialog.component';
 import { MatDialogModule } from '@angular/material';
 
 @NgModule({
   declarations: [
-    HeaderMenuComponent
+    HeaderMenuComponent, ChartsDirective
   ],
-  exports: [HeaderMenuComponent, ShortcutHelpDialogComponent],
+  exports: [HeaderMenuComponent, ShortcutHelpDialogComponent, ChartsDirective],
   imports: [
     MatDialogModule,
     BrowserModule,
